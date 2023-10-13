9e75e1fb5430d2cf0f5fb699ae4321576ee62727
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-10-13 21:04:17 +0530

    user.service

---
+++ service/user.service.js
@@ -0,0 +1,25 @@
+const user = require('../dao/user.dao');
+
+const registerService = async(first_name, last_name, username, password , email, phone,created,updated) => {
+    return await user.registerUser(first_name, last_name, username, password , email, phone,created,updated)
+}
+
+const existsUserService = async(email,username)=>{
+    return await user.existsUser(email,username) ; 
+} 
+
+const getAllUsersService = async() => {
+    return await user.getAllUsers() ; 
+}
+
+const loginService = async(email,password) => {
+    return await user.login(email,password) ; 
+}
+
+
+module.exports = {
+    registerService,
+    existsUserService,
+    getAllUsersService,
+    loginService
+}

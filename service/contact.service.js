7e436818664d086685faf76565a4c1c3682a7677
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-10-14 14:36:20 +0530

    contact service

--- service/contact.service.js
+++ service/contact.service.js
@@ -1,13 +1,19 @@
 const contact = require('../dao/contact.dao');
 
+
+//insert contact service 
 const insertcontactService = async(fullname, address, contactno, zip, email, user_id, created, updated) => {
     return await contact.insertContact(fullname, address, contactno, zip, email, user_id, created, updated)
 }
+
+//contact details API with user details service
 
 const getDetailsService = async() => {
     return await contact.getDetails() ; 
 }
 
+
+// user details by user id along with all contacts of that user Service
 const getContactByidService = async(user_id) => {
     return await contact.getContactByUserId(user_id) ; 
 }
